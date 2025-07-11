b51c5df9d9bea2c2c1fc083e8024c6c34be64d88
Leonid Tsypkin <caudate7855@gmail.com>
2025-07-11 10:48:38 +0700

    - feat: json utils created

---
+++ Scripts/Utils/JsonUtils.cs
@@ -0,0 +1,20 @@
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace PlaceHolderSDK
+{
+    internal static class JsonUtils
+    {
+        public static T Deserialize<T>(Stream stream)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            return (T)serializer.ReadObject(stream);
+        }
+
+        public static void Serialize<T>(T obj, Stream stream)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            serializer.WriteObject(stream, obj);
+        }
+    }
+}
